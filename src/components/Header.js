043173f9ce8c354fd225f7b803727b13078227a8
meliphyra-ux <xennerit@icloud.com>
2022-09-11 00:58:53 +0300

    Changed styles and burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from '../assets/styles/header.module.css'
+import menu from '../assets/images/menu.png'
 
 export default function Header() {
+  const [toggleNavigation, setToggleNavigation] = useState(false);
   return (
     <header className={styles.header}>
         {/* Here will be my logo */}
         <h1>My portfolio</h1>
-        <nav>
+        <nav className={toggleNavigation === true ? styles.active : ""}>
             <a href='https://github.com/meliphyra-ux'>About me</a>
             <a href='https://github.com/meliphyra-ux'>My projects</a>
             <a href='https://github.com/meliphyra-ux'>My contacts</a>
         </nav>
-        <button>Burger</button>
+        <img src={menu} onClick={()=>setToggleNavigation(!toggleNavigation)} alt="burger icon"/>
     </header>
   )
 }
